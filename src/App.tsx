8b38fcab83d5e2d5abf45803a1f85523f726cf38
nohseongbong <shtjdqhd@naver.com>
2024-01-04 19:54:45 +0900

    feat: recoil root 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {SafeAreaView, StatusBar, useColorScheme} from 'react-native';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import MainScreen from '@/screen/main';
+import {RecoilRoot} from 'recoil';
 
 const queryClient = new QueryClient();
 
@@ -9,12 +10,14 @@ function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
-    <SafeAreaView>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <QueryClientProvider client={queryClient}>
-        <MainScreen />
-      </QueryClientProvider>
-    </SafeAreaView>
+    <RecoilRoot>
+      <SafeAreaView>
+        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+        <QueryClientProvider client={queryClient}>
+          <MainScreen />
+        </QueryClientProvider>
+      </SafeAreaView>
+    </RecoilRoot>
   );
 }
 
