e18cb0ffc744d64f33e192f61e0b32b05a816ade
nohseongbong <shtjdqhd@naver.com>
2024-01-06 19:14:46 +0900

    refactor: 불필요한 코드 제거

--- src/screen/main.tsx
+++ src/screen/main.tsx
@@ -6,9 +6,6 @@ import usePost from '@/hooks/usePost';
 
 function MainScreen() {
   const {posts, isLoading, isError} = usePost();
-  useEffect(() => {
-    console.log('부모 컴포넌트 렌더링');
-  }, []);
 
   if (isLoading) {
     return <Text>Loading...</Text>;
